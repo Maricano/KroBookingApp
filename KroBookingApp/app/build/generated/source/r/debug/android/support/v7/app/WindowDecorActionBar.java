7bb59807dd5436f310f233039dc8c1c620d7d83f
Kenneth Jørgensen <Kenneth Jrgensen>
2016-04-27 10:56:54 +0200

    Fresh Start

--- KroBookingApp/app/build/generated/source/r/debug/android/support/v7/app/WindowDecorActionBar.java
+++
@@ -1,7 +0,0 @@
-package android.support.v7.app;
-
-/**
- * Created by kenne on 27-04-2016.
- */
-public class WindowDecorActionBar {
-}
--- KroBookingApp/app/src/main/java/com/example/kenne/krobookingapp/RestService/RestService.java
+++
@@ -1,7 +0,0 @@
-package com.example.kenne.krobookingapp.RestService;
-
-/**
- * Created by kenne on 27-04-2016.
- */
-public class RestService {
-}
